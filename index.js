f93e4f0697f7d00fa8a98d3aef0e666080229257
DecrepitSloth <knowlesaa77@gmail.com>
2024-04-14 01:24:37 -0500

    select child of .inventory and and place new divs inside addItem

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ document.getElementById("add").addEventListener("click", addItem);
 
 function addItem() {
     const newDiv = document.createElement("div");
-    const list = document.getElementsByClassName("inventory");
-    document.body.appendChild(newDiv);
+    const list = document.querySelector(".inventory div:nth-child(1)");
+    list.appendChild(newDiv);
     newDiv.className = "test";   
-}
+};
